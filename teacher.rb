1787f7e9b1ea9254ea2b6bf61a9c494c9d2d8800
Ikonde Nekemiah Arnold <i-nad@localhost.localdomain>
2023-10-03 12:23:47 +0300

    Added person, student and teacher classes

---
+++ teacher.rb
@@ -0,0 +1,15 @@
+require './person'
+attr_reader :specialization
+
+# The is the teacher class, which represents teacher information
+class Teacher < Person
+  def initialize(specialization:, name: 'Unknown', age: 0)
+    super(name, age)
+    @specialization = specialization
+  end
+
+  # override the can_use_services? method
+  def can_use_services?
+    true
+  end
+end
