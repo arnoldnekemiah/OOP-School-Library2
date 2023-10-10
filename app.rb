7cdcd0ff3b3a84667d59ecc7c264282291866c10
Wils-FOURR-speed <124159862+Wils-FOURR-speed@users.noreply.github.com>
2023-10-10 11:15:23 +0100

    Apply changes from code reviewer

--- app.rb
+++ app.rb
@@ -38,24 +38,6 @@ class App
     puts '************************************************************'
   end
 
-  def create_person
-    puts "Choose the type of person:\n1. Student\n2. Teacher"
-    print 'Enter the type (1 or 2): '
-    type = gets.chomp.to_i
-
-    if type == 1
-      student = @person_factory.create_student
-      @people << student unless student.nil?
-      puts "Student #{student.name} created."
-    elsif type == 2
-      teacher = @person_factory.create_teacher
-      @people << teacher unless teacher.nil?
-      puts "Teacher #{teacher.name} created."
-    else
-      puts 'Invalid choice,person not created.'
-    end
-  end
-
   def create_book
     print 'Enter the title of the book: '
     title = gets.chomp
--- person_factory.rb
+++ person_factory.rb
@@ -1,6 +1,24 @@
 require_relative 'teacher'
 require_relative 'student'
 class PersonFactory
+  def create_person
+    puts "Choose the type of person:\n1. Student\n2. Teacher"
+    print 'Enter the type (1 or 2): '
+    type = gets.chomp.to_i
+
+    if type == 1
+      student = @person_factory.create_student
+      @people << student unless student.nil?
+      puts "Student #{student.name} created."
+    elsif type == 2
+      teacher = @person_factory.create_teacher
+      @people << teacher unless teacher.nil?
+      puts "Teacher #{teacher.name} created."
+    else
+      puts 'Invalid choice,person not created.'
+    end
+  end
+
   def create_student()
     print 'Name: '
     name = gets.chomp
