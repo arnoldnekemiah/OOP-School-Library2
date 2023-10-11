63e66be315f85394691476c5b74234dc471ab98e
Wils-FOURR-speed <124159862+Wils-FOURR-speed@users.noreply.github.com>
2023-10-11 13:32:42 +0100

    Fix linters

--- Data/data_manager.rb
+++ Data/data_manager.rb
@@ -1,44 +1,44 @@
 require 'json'
 
 class ManageData
-    attr_accessor :books, :person, :rentals
+  attr_accessor :books, :person, :rentals
 
-    def initialize
-        @books = []
-        @person = []
-        @rentals = []
-    end
+  def initialize
+    @books = []
+    @person = []
+    @rentals = []
+  end
 
-    def load_data
-        load_books
-        load_person
-        load_rentals
+  def load_data
+    load_books
+    load_person
+    load_rentals
+  end
+
+  def save_data
+    save_books
+    puts 'Book Saved.'
+  rescue StandardError => e
+    puts "An error occured while saving data: #{e.message}"
+  end
+
+  def save_books
+    File.open('books.json', 'w') do |file|
+      file.puts @books.map { |book|
+        { 'title' => book.title,
+          'author' => book.author }
+      }.to_json
     end
-    
-    def save_data
-        begin
-            save_books
-            puts "Book Saved."
-        rescue StandardError => e
-            puts "An error occured while saving data: #{e.message}"
-        end
-    end
-    
-    def save_books
-        File.open('books.json', 'w') do |file|
-            file.puts @books.map { |book|
-             { 'title' => book.title,
-             'author' => book.author }
-            }.to_json
-        end
-    end
+  end
 
   private
-def load_books
+
+  def load_books
     return unless File.exist?('books.json')
+
     json_str = File.read('books.json')
     @books = JSON.parse(json_str).map do |book_data|
-        Book.new(book_data['title'], book_data['author'])
+      Book.new(book_data['title'], book_data['author'])
     end
-end    
-end     
+  end
+end
