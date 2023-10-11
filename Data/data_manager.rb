20dad15693b31c3485feb2134c74ad7cf983c1de
Wils-FOURR-speed <124159862+Wils-FOURR-speed@users.noreply.github.com>
2023-10-11 14:55:16 +0100

    Add rentals save

--- Data/data_manager.rb
+++ Data/data_manager.rb
@@ -37,60 +37,62 @@ class ManageData
     File.open('people.json', 'w') do |file|
       file.puts @people.map { |person|
         {
-           id: person.id,
-          name: person.name
-          age: person.age
-          type: person.class
-          rentals: person.rentals
-          parent_permission: person.parent_permission
-          specialization: specialization_check(person)}
-      }.to_json
-    end
-
-    def specialization_check(person)
-      if person.instance_of? (Teacher)
-        person.specialization 
-      else
-        'No specialization'
-      end
-    end
-  end
-
-  def save_rentals
-    File.open('rentals.json', 'w') do |file|
-      file.puts @rentals.map { |rental|
-        { 'date' => rental.date,
-          'book' => rental.book,
-          'person' => rental.person }
+          id: person.id,
+          name: person.name,
+          age: person.age,
+          type: person.class,
+          rentals: person.rentals,
+          parent_permission: person.parent_permission,
+          specialization: specialization_check(person)
+        }
       }.to_json
     end
   end
+
+  def specialization_check(person)
+    if person.instance_of?(Teacher)
+      person.specialization
+    else
+      'No specialization'
+    end
+  end
+end
+
+def save_rentals
+  File.open('rentals.json', 'w') do |file|
+    file.puts @rentals.map { |rental|
+      { 'date' => rental.date,
+        'book' => rental.book,
+        'person' => rental.person }
+    }.to_json
+  end
+end
 
   private
 
-  def load_books
-    return unless File.exist?('books.json')
+def load_books
+  return unless File.exist?('books.json')
 
-    json_str = File.read('books.json')
-    @books = JSON.parse(json_str).map do |book_data|
-      Book.new(book_data['title'], book_data['author'])
+  json_str = File.read('books.json')
+  @books = JSON.parse(json_str).map do |book_data|
+    Book.new(book_data['title'], book_data['author'])
+  end
+end
+
+def load_people(data)
+  data.each do |person_data|
+    if person_data['type'] == 'Student'
+      student = Student.new(person_data['age'], person_data['name'],
+                            parent_permission: person_data['parent_permission'])
+      student.instance_variable_set(:@id, person_data['id'])
+      @people << student
+    else
+      teacher = Teacher.new(person_data['specialization'], person_data['age'], person_data['name'])
+      teacher.instance_variable_set(:@id, person_data['id'])
+      @people << teacher
     end
   end
-
-  def load_people(data)
-    data.each do |person_data|
-      if person_data['type'] == 'Student'
-        student = Student.new(person_data['age'], person_data['name'], parent_permission: person_data['parent_permission'])
-        student.instance_variable_set(:@id, person_data['id'])
-        @people << student
-      else
-        teacher =Teacher.new(person_data['specialization'], person_data['age'], person_data['name'])
-        teacher.instance_variable_set(:@id, person_data['id'])
-        @people << teacher
-      end
-  end
   @people
-  end
-
-  def load_rentals
 end
+
+def load_rentals; end
--- app.rb
+++ app.rb
@@ -8,6 +8,7 @@ require_relative 'book'
 require_relative 'classroom'
 require_relative 'book_loader'
 require_relative 'rental_manager'
+require_relative 'data_manager'
 
 class App
   def initialize
@@ -16,54 +17,64 @@ class App
     @rentals = []
     @book_loader = BooksLoader.new
     @rental_manager = RentalManager.new
+    @data_manager = ManageData.new
   end
 
-  def list_books
-    puts '************************************************************'
-    puts "List of Books (#{@books.length}):"
-    @books.each do |book|
-      puts "#{book.id} - #{book.title} by #{book.author}"
+  def load_data
+    @data_manager.load_data
+    @books = @data_manager.books
+    @people = @data_manager.people
+    @rentals = @data_manager.rentals
+  end
+
+  def save_data
+    @data_manager.save_books
+    puts 'Book saved successfully.'
+    @data_manager.save_people
+    puts 'Person saved successfully.'
+    @data_manager.save_rentals
+    puts 'Rental saved successfully.'
+
+    def list_books
+      puts '************************************************************'
+      puts "List of Books (#{@books.length}):"
+      @books.each do |book|
+        puts "#{book.id} - #{book.title} by #{book.author}"
+      end
+      puts '************************************************************'
     end
-    puts '************************************************************'
-  end
 
-  def list_people
-    puts '************************************************************'
-    puts "List of People (#{@people.length}):"
-    @people.each do |person|
-      puts "#{person.id} - #{person.name} (#{person.is_a?(Student) ? 'Student' : 'Teacher'})"
+    def list_people
+      puts '************************************************************'
+      puts "List of People (#{@people.length}):"
+      @people.each do |person|
+        puts "#{person.id} - #{person.name} (#{person.is_a?(Student) ? 'Student' : 'Teacher'})"
+      end
+      puts '************************************************************'
     end
-    puts '************************************************************'
-  end
 
-  def create_student
-    student = Student.create_student
-    @people << student unless student.nil?
-    puts "Student #{student.name} created."
-  end
+    def create_student
+      student = Student.create_student
+      @people << student unless student.nil?
+      puts "Student #{student.name} created."
+    end
 
-  def create_teacher
-    teacher = Teacher.create_teacher
-    @people << teacher unless teacher.nil?
-    puts "Teacher #{teacher.name} created."
-  end
+    def create_teacher
+      teacher = Teacher.create_teacher
+      @people << teacher unless teacher.nil?
+      puts "Teacher #{teacher.name} created."
+    end
 
-  def create_book
-    print 'Enter the title of the book: '
-    title = gets.chomp
-    print 'Enter the author of the book: '
-    author = gets.chomp
+    def create_book_wrapper
+      create_book.call
+    end
 
-    book = Book.new(title, author)
-    @books << book
-    puts "Book '#{book.title}' by #{book.author} created."
-  end
+    def create_rental_wrapper(people, books, _rentals)
+      create_rental.call(people, books, _rentals)
+    end
 
-  def create_rental
-    @rental_manager.create_rental(@people, @books, @rentals)
-  end
-
-  def list_rentals_for_person
-    @rental_manager.list_rentals_for_person(@people)
+    def list_rentals_for_person_wrapper(people)
+      list_rentals_for_person.call(people)
+    end
   end
 end
