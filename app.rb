a1486fb5cbc7fc5825d54aea999bb5a3afe6ec28
Ikonde Nekemiah Arnold <i-nad@localhost.localdomain>
2023-10-12 15:34:59 +0300

    fixed json

--- app.rb
+++ app.rb
@@ -1,94 +1,139 @@
+require 'json'
 require_relative 'person'
 require_relative 'student'
-require_relative 'teacher'
-require_relative 'capitalize_decorator'
-require_relative 'trimmer_decorator'
 require_relative 'rental'
 require_relative 'book'
+require_relative 'teacher'
 require_relative 'classroom'
-require_relative 'book_loader'
-require_relative 'rental_manager'
-require_relative 'people_loader'
-require_relative 'books_loader'
-require_relative 'rentals_loader'
+require_relative 'data_store'
 
 class App
-  attr_reader :people, :books, :rentals
-
+  include DataStore
   def initialize
-    @people = PeopleLoader.load || []
-    @books = BooksLoader.load || []
-    @rentals = RentalsLoader.load || []
-    @book_loader = BooksLoader.new
-    @rental_manager = RentalManager.new
-
-    create_missing_data_files
+    @books = load_books
+    @people = load_people
+    @rentals = load_rentals
   end
 
-  def list_books
-    puts '************************************************************'
-    puts "List of Books (#{@books.length}):"
-    @books.each do |book|
-      if book.is_a?(Book)
-        puts "#{book.id} - #{book.title} by #{book.author}"
-      else
-        puts "Invalid book object: #{book}"
-      end
+  def get_num(option)
+    case option
+    when '1'
+      list_all_books
+    when '2'
+      list_all_people
+    when '3'
+      create_person
+    when '4'
+      create_book
+    when '5'
+      create_rental
+    when '6'
+      list_rentals
+    else
+      puts 'Please enter a number between 1 and 7'
     end
-    puts '************************************************************'
   end
 
-  def list_people
-    puts '************************************************************'
-    puts "List of People (#{@people.length}):"
-    @people.each do |person|
-      puts "#{person.id} - #{person.name} (#{person.is_a?(Student) ? 'Student' : 'Teacher'})"
+  def list_all_books
+    puts 'There are no books yet! Kindly add books.' if @books.empty?
+    @books.each { |book| puts "Title: #{book.title}, Author: #{book.author}" }
+    sleep 0.75
+  end
+
+  def list_all_people
+    puts 'There are no people yet! Kindly add a student or teacher.' if @people.empty?
+    @people.map { |person| puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}" }
+    sleep 0.75
+  end
+
+  def create_person
+    print 'Do you want to create a student (1) or teacher (2) [Input a number]: '
+    option = gets.chomp
+
+    case option
+    when '1'
+      create_student
+    when '2'
+      create_teacher
+    else
+      puts 'Invalid input. Kindly type 1 or 2'
     end
-    puts '************************************************************'
   end
 
   def create_student
-    student = Student.create_student
-    if student
-      @people << student
-      puts "Student #{student.name} created."
-    else
-      puts 'Failed to create a student.'
-    end
+    print 'Age: '
+    age = gets.chomp
+
+    print 'Name: '
+    name = gets.chomp
+
+    print 'Has parent permission? [Y/N]: '
+    parent_permission = gets.chomp.downcase == 'y'
+
+    student = Student.new(age: age, name: name, parent_permission: parent_permission, classroom: @classroom)
+    @people << student
+
+    puts 'Student created successfully'
+    sleep 0.75
   end
 
   def create_teacher
-    teacher = Teacher.create_teacher
-    @people << teacher unless teacher.nil?
-    puts "Teacher #{teacher.name} created."
+    print 'Age: '
+    age = gets.chomp.to_i
+
+    print 'Name: '
+    name = gets.chomp
+
+    print 'Specialization: '
+    specialization = gets.chomp
+
+    teacher = Teacher.new(age: age, name: name, specialization: specialization)
+
+    @people << teacher
+
+    puts 'Teacher created successfully'
+    sleep 0.75
   end
 
   def create_book
-    print 'Enter the title of the book: '
+    print 'Title: '
     title = gets.chomp
-    print 'Enter the author of the book: '
+
+    print 'Author: '
     author = gets.chomp
 
     book = Book.new(title, author)
     @books << book
-    puts "Book '#{book.title}' by #{book.author} created."
+
+    puts 'Book created successfully'
   end
 
   def create_rental
-    @rental_manager.create_rental(@people, @books, @rentals)
+    puts 'Select a book from the following list by number'
+    @books.each_with_index { |book, index| puts "#{index}) Title: #{book.title}, Author: #{book.author}" }
+    book_i = gets.chomp.to_i
+    puts
+    puts 'Select a person from the following list by number (not ID)'
+    @people.each_with_index do |person, index|
+      puts "#{index}) [#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+    end
+    person_i = gets.chomp.to_i
+    puts
+    print 'Date: '
+    date = gets.chomp
+    rental = Rental.new(date, @books[book_i], @people[person_i])
+    @rentals.push(rental)
+    puts 'Rental created successfully'
   end
 
-  def list_rentals_for_person
-    @rental_manager.list_rentals_for_person(@people)
-  end
+  def list_rentals
+    print 'ID of person: '
+    id = gets.chomp
 
-  def create_missing_data_files
-    File.write('people.json', [].to_json) unless File.exist?('people.json')
-
-    File.write('books.json', [].to_json) unless File.exist?('books.json')
-
-    return if File.exist?('rentals.json')
-
-    File.write('rentals.json', [].to_json)
+    puts 'Rentals:'
+    @rentals.each do |rental|
+      puts "Date: #{rental.date}, Book '#{rental.book.title}' by #{rental.book.author}" if rental.person.id == id.to_i
+    end
+    sleep 0.75
   end
 end
--- student.rb
+++ student.rb
@@ -1,37 +1,19 @@
 require_relative 'person'
 
-# This is student class, which represents student's information
 class Student < Person
   attr_reader :classroom
 
-  def initialize(classroom:, name: 'Unknown', age: 0, parent_permission: true)
-    super(name: name, age: age, parent_permission: parent_permission)
+  def initialize(age:, classroom:, id: nil, name: 'Unknown', parent_permission: true)
+    super(id: id, name: name, age: age, parent_permission: parent_permission)
     @classroom = classroom
   end
 
-  def play_hook
-    '¯(ツ)/¯'
-  end
-
-  def self.create_student
-    print 'Name: '
-    name = gets.chomp
-    print 'Classroom: '
-    classroom = gets.chomp.to_str
-    return nil unless classroom.length >= 1
-
-    print 'Age (numeric): '
-    age = gets.chomp.to_i
-    return nil unless age.positive?
-
-    print 'Parent permission (y for true or n for false): '
-    parent_permission = gets.chomp
-
-    Student.new(classroom: classroom, age: age, name: name, parent_permission: parent_permission.downcase == 'y')
-  end
-
-  def add_to_classroom(classroom)
+  def classroom=(classroom)
     @classroom = classroom
     classroom.students.push(self) unless classroom.students.include?(self)
+  end
+
+  def play_hooky
+    "¯\(ツ)/¯"
   end
 end
